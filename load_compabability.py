6556504163e0e7a4e4894e6be15d908594594610
Ivan Bezgin <ivanbezgin@MacBook-Pro-Ivan.local>
2024-05-21 23:05:44 +0300

    build json file

--- load_compabability.py
+++ load_compabability.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+HTML_PARSER = "html.parser"
+
+URL = "https://horoscopes.rambler.ru/sovmestimost-znakov-zodiaka/"
 
 ALL_SEX = ["МУЖЧИНА", "ЖЕНЩИНА"]
-
 ALL_ZODIAC = [
     "ОВЕН",
     "ТЕЛЕЦ",
@@ -21,25 +23,40 @@ ALL_ZODIAC = [
 
 FRAME_CLASS = "j+xoX"
 CHART_CLASS = "PM-qP"
-
-HTML_PARSER_STR = "html.parser"
+PERSENT_CLASS = "THo-S"
 
 
 def start():
-    url = "https://horoscopes.rambler.ru/sovmestimost-znakov-zodiaka/"
-    response = requests.get(url)
-    page_soup = BeautifulSoup(response.text, HTML_PARSER_STR)
+    response = requests.get(URL, timeout=30000)
+    page_soup = BeautifulSoup(response.text, HTML_PARSER)
     all_frames = page_soup.find_all("div", class_=FRAME_CLASS)
     counter = 0
+    result_json = ""
     for sex in ALL_SEX:
+        sex_str = "{" + f'"{sex}"' + ":"
+        result_json += sex_str
         for zodiac in ALL_ZODIAC:
+            zodiac_str = "{" + f'"{zodiac}"' + ":{"
+            result_json += zodiac_str
             current_frame = str(all_frames[counter])
-            current_frame_poup = BeautifulSoup(current_frame, HTML_PARSER_STR)
-            all_zodiac_chart = current_frame_poup.find_all("a", class_=CHART_CLASS)
-            print(len(all_zodiac_chart))
-            print(len(ALL_ZODIAC))
-            print("-------------------")
+            current_frame_soup = BeautifulSoup(current_frame, HTML_PARSER)
+            all_zodiac_chart = current_frame_soup.find_all("a", class_=CHART_CLASS)
+            zodiac_counter = 0
+            for chart in all_zodiac_chart:
+                chart_soup = BeautifulSoup(str(chart), HTML_PARSER)
+                persent_span = chart_soup.find("span", class_=PERSENT_CLASS)
+                persent_str = persent_span.get_text()
+                current_zodiac = ALL_ZODIAC[zodiac_counter]
+                print(persent_str)
+                current_zodiac_string = f'"{current_zodiac}":"{persent_str}",'
+                result_json += current_zodiac_string
+                zodiac_counter += 1
             counter += 1
+            result_json += "},"
+        result_json += "},"
+
+    print(result_json)
+    print(counter)
 
 
 start()
